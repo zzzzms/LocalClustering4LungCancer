30338723868e3fa5119dcf553b6cbef65f46b4af
zzzzms <67567262+zzzzms@users.noreply.github.com>
2026-06-15 09:45:40 -0700

    Update main_CS_LCE.m

--- LCE4LungCancer/main_CS_LCE.m
+++ LCE4LungCancer/main_CS_LCE.m
@@ -1,26 +1,5 @@
 function Cluster = main_CS_LCE(A,Gamma,n0,epsilon,t,reject)
 
-% ================================================================= %
-% This program implements the LCE Algorithm.
-
-% ========================= Acknowledgement =============================
-% It is modified based on Dr. Daniel Mckenzie's original code. 
-% Zhaiming Shen. April 2023
-% =======================================================================
-
-
-% INPUT
-% =================================
-% A .................... Adjacency matrix of data converted to graph form.
-% Gamma ................ VECTOR. Labelled data within cluster of interest
-% n0 ................... (estimated) size of C_a
-% epsilon .............. Omega_a will be of size (1+epsilon)n0(a)
-% t .................... Depth of random walk 
-%
-% OUTPUT
-% ================================
-% Cluster...... VECTOR. The elements in the cluster of interest.
-%
 
 % ========= Initialization ================= %
 n = size(A,1); % number of vertices
